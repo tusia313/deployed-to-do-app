234b881b97f009dd08ab4bab4fd5a9de5eb53a35
Marta Łoskot <loskotmarta@gmail.com>
2025-10-01 13:37:27 +0200

    Styling the list items

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,8 +1,15 @@
 const ListHeader = ({listName}) => {
+  const signOut = () => {
+    console.log("signing out")
+  }
   return (
     <div className="list-header">
   {/* dzięki {} listName jest tarktowane jako KOD a nie tekst */}
       <h1>{listName}</h1>
+      <div className="button-container">
+        <button className="create">add new</button>
+        <button className="signout" onClick={signOut}>sign out</button>
+      </div>
     </div>
   )
 }
