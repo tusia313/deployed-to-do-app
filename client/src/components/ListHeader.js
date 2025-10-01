0b48e41ffbadb7e6ba5a934e8a7903286d4a31c3
Marta Łoskot <loskotmarta@gmail.com>
2025-10-01 15:04:58 +0200

    Insert some example data in SQL

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,11 +1,14 @@
-const ListHeader = ({listName}) => {
+const ListHeader = ({ listName }) => {
+  const date = new Date().toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' })
+  console.log(Date)
   const signOut = () => {
     console.log("signing out")
   }
   return (
     <div className="list-header">
-  {/* dzięki {} listName jest tarktowane jako KOD a nie tekst */}
+      {/* dzięki {} listName jest tarktowane jako KOD a nie tekst */}
       <h1>{listName}</h1>
+      <h2>{date}</h2>
       <div className="button-container">
         <button className="create">add new</button>
         <button className="signout" onClick={signOut}>sign out</button>
---
+++ server/data.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE todo_app;
+
+CREATE TABLE todos (
+    id varchar(36) PRIMARY KEY,
+    user_email varchar(255),
+    title varchar(255),
+    progress int(50),
+    date varchar(505)
+);
+
+CREATE TABLE users (
+    email varchar(255) PRIMARY KEY,
+    hashed_password varchar(255)
+);
+-- Data for example 
+INSERT INTO todos (id, user_email, title, progress, date) VALUES
+('0','loskotmarta@gmail.com','Learn to code',95,'01 Oct 2025'),
