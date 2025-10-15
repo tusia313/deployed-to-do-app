397841dee21f9685f7d1b3fd31651a73d25be5a4
Marta Łoskot <loskotmarta@gmail.com>
2025-10-15 22:05:14 +0200

    Get database with unique email

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import ListHeader from './components/ListHeader'
+import { useEffect } from 'react'
+
 const App = () => {
+  const userEmail = 'loskotmarta@gmail.com'
+  const getData = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
+      const json = await response.json()
+      console.log(json)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => { getData() }, [])
+
   return (
     <div className="app">
-      <ListHeader listName = {"🎄 December tick list"}/>
+      <ListHeader listName={"🎄 December tick list"} />
     </div>
   )
 }
+
 export default App
--- server/server.js
+++ server/server.js
@@ -1,16 +1,22 @@
 const PORT = process.env.PORT ?? 8000
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const pool = require('./db')
 
+app.use(cors())
+
 // that is routing!
-app.get('/', (req, res) => {
-    res.send('Hello Marta!')
-})
+// app.get('/todos', (req, res) => {
+//     res.send('Hello Marta!')
+// })
+
 // get all todos
-app.get('/todos', async (req, res) => {
+app.get('/todos/:userEmail', async (req, res) => {
+    const userEmail = req.params.userEmail
+    console.log(userEmail)
     try {
-        const todos = await pool.query('SELECT * FROM todos')
+        const todos = await pool.query('SELECT * FROM todos WHERE user_email=$1', [userEmail])
         res.json(todos.rows)
     } catch (error) {
         console.error(error)
