8621956358c9507ad9ca6c465e3c776ec0add259
Marta Łoskot <loskotmarta@gmail.com>
2025-10-20 13:31:16 +0200

    Savings data at todos

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import ListHeader from './components/ListHeader'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const App = () => {
   const userEmail = 'loskotmarta@gmail.com'
+  const [tasks, setTasks] = useState()
   const getData = async () => {
     try {
       const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
       const json = await response.json()
-      console.log(json)
+      setTasks(json)
     } catch (error) {
       console.error(error)
     }
   }
 
   useEffect(() => { getData() }, [])
+  console.log("Wyświetlam json: ", tasks)
+  //sort yasks by date
+  tasks?.sort((a, b) => new Date(a.date) - new Date(b.date)) 
 
   return (
     <div className="app">
