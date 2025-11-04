9772bbdd9f0dfcd9944bb79f0b895fb913f5abe2
Marta Łoskot <loskotmarta@gmail.com>
2025-11-04 21:06:18 +0100

    Finally added a proper version of a range

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,8 +1,24 @@
+import { useState } from "react"
 const Modal = () => {
-  const handleChange = () => {
-    console.log("handlimg changes!")
-  }
   const mode = "create"
+  const editMode = mode === "edit" ? true : false
+  const [data, setData] = useState({
+    user_email:"",
+    title:"",
+    progress: "",
+    date: editMode ? "" : new Date()
+  })
+  const handleChange = (e) => {
+    // jakby to jest to samo co oddzielnie nazwac e.target.name i e.target.value
+    const {name, value} = e.target
+    setData(data => ({
+      ...data,
+      // kochane algorytmy : teraz nadpisujemy obiekt data, ale tylko to co sie zmienilo
+      [name]: value
+    }))
+    console.log("To sa teraz nasze dane: " , data)
+  }
+  
   return (
     <div className="overlay">
       <div className="modal">
@@ -16,16 +32,21 @@ const Modal = () => {
             maxLength={30}
             placeholder="you're task goes here..."
             name="title"
-            value={""}
+            value={data.title}
             onChange={handleChange}
           />
+          <label for="range">Drag to select Your current progress</label>
           <input
             type="range"
             name="progress"
+            id="range"
             min={0}
             max={100}
-            value={""}
+            value={data.progress}
             onChange={handleChange}
+    style={{
+    accentColor: "rgb(50, 200, 100)"
+  }}
           />
           <input
             className={mode}
