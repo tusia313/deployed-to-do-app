442c737b6638a02d987ab0ae78e042d1594b807e
Marta Łoskot <loskotmarta@gmail.com>
2025-11-06 23:46:46 +0100

    Posting data

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,29 +1,44 @@
 import { useState } from "react"
-const Modal = ({mode, setShowModal, task}) => {
+const Modal = ({ mode, setShowModal, task }) => {
   const editMode = mode === "edit" ? true : false
   const [data, setData] = useState({
-    user_email: editMode ? task.user_email : "",
-    title: editMode ? task.title :"",
+    user_email: editMode ? task.user_email : "bob.test@gmail.com",
+    title: editMode ? task.title : "",
     progress: editMode ? task.progress : 50,
     date: editMode ? "" : new Date()
   })
   const handleChange = (e) => {
     // jakby to jest to samo co oddzielnie nazwac e.target.name i e.target.value
-    const {name, value} = e.target
+    const { name, value } = e.target
     setData(data => ({
       ...data,
       // kochane algorytmy : teraz nadpisujemy obiekt data, ale tylko to co sie zmienilo
       [name]: value
     }))
-    console.log("To sa teraz nasze dane: " , data)
+    console.log("To sa teraz nasze dane: ", data)
   }
-  
+  const postData = async (e) => {
+    e.preventDefault()
+    try {
+      const response = fetch('http://localhost:8000/todos', {
+        method : 'POST',
+        headers: {
+          'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({data})
+        })
+        console.log("Response from posting data in Modal.js: ", response)
+    } catch (error) {
+      console.error("Error posting data: ", error)
+    }
+  }
+
   return (
     <div className="overlay">
       <div className="modal">
         <div className="form-title-container">
           <h2>Let's {mode} some task!</h2>
-          <button onClick={()=> setShowModal(false)}>x</button>
+          <button onClick={() => setShowModal(false)}>x</button>
         </div>
         <form>
           <input
@@ -43,13 +58,14 @@ const Modal = ({mode, setShowModal, task}) => {
             max={100}
             value={data.progress}
             onChange={handleChange}
-    style={{
-    accentColor: "rgb(50, 200, 100)"
-  }}
+            style={{
+              accentColor: "rgb(50, 200, 100)"
+            }}
           />
           <input
             className={mode}
             type="submit"
+            onClick = {editMode ? '' : postData}
           />
         </form>
       </div>
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const pool = require('./db')
+const {v4: uuidv4} = require('uuid')
 
 app.use(cors())
 
@@ -14,10 +15,22 @@ app.use(cors())
 // get all todos
 app.get('/todos/:userEmail', async (req, res) => {
     const userEmail = req.params.userEmail
-    console.log(userEmail)
     try {
         const todos = await pool.query('SELECT * FROM todos WHERE user_email=$1', [userEmail])
         res.json(todos.rows)
+    } catch (error) {
+        console.error(error)
+    }
+})
+// create a new todo, todos is an endpoint; get request might be as well
+app.post('/todos', (req, res) => {
+    const {user_email, title, progress, date} = req.body
+    console.log("To sa dane z server.js: ", req.body)
+    uuidv4()  // generuje nam unikalne id
+    try {
+        pool.query(`INSERT INTO todos (id, user_email, title, progress, date) VALUES ($1, $2, $3, $4, $5)`
+        [id, user_email, title, progress, date]
+        )
     } catch (error) {
         console.error(error)
     }
